340d2788f5f375de91e6e3e95c70ef8b65fdc363
Vitoria <vitoriajemima2007@gmail.com>
2024-06-25 15:49:36 -0300

    Tela de Fale conosco finalizdada

---
+++ app/src/main/java/com/example/tccmobile/FaleConosco.java
@@ -0,0 +1,17 @@
+package com.example.tccmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaleConosco extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fale_conosco);
+
+    }
+}
