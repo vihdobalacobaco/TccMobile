63bb29d10b840380fd69a2df54e1a7b61ff9a3ef
vihh <125580286+vihdobalacobaco@users.noreply.github.com>
2024-06-27 18:26:51 -0300

    Conexão entre a tela login e tela cadastro

--- app/src/main/java/com/example/tccmobile/FormCadastro.java
+++ app/src/main/java/com/example/tccmobile/FormCadastro.java
@@ -2,13 +2,31 @@ package com.example.tccmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormCadastro extends AppCompatActivity {
+
+    private TextView text_tenho_cad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        IniciarComponentes();
+        text_tenho_cad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormCadastro.this, FormLogin.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    private void IniciarComponentes(){
+        text_tenho_cad = findViewById(R.id.text_tenho_cad);
+    }
+    }
--- app/src/main/java/com/example/tccmobile/FormLogin.java
+++ app/src/main/java/com/example/tccmobile/FormLogin.java
@@ -2,15 +2,31 @@ package com.example.tccmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
-    }
 
+        IniciarComponentes();
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
 }
