1d777316803a48829437d68013c1f35bd9e5078b
Eduardo Lima <125580660+Synced8981@users.noreply.github.com>
2024-06-27 13:44:33 -0300

    Tela cadastro
    Atualização e criação da tela cadastro

--- app/src/main/java/com/example/tccmobile/FormCadastro.java
+++ app/src/main/java/com/example/tccmobile/FormCadastro.java
@@ -3,14 +3,12 @@ package com.example.tccmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class FormLogin extends AppCompatActivity {
+public class FormCadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_login);
+        setContentView(R.layout.activity_form_cadastro);
     }
-
 }
