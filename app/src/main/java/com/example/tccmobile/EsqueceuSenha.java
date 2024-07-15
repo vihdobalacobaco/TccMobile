c707f24d180e8663cb281e3bfe99accb1cae77c2
vihh <125580286+vihdobalacobaco@users.noreply.github.com>
2024-07-15 17:50:51 -0300

    Tela Redefinir senha

---
+++ app/src/main/java/com/example/tccmobile/EsqueceuSenha.java
@@ -0,0 +1,14 @@
+package com.example.tccmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EsqueceuSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esqueceu_senha);
+    }
+}
