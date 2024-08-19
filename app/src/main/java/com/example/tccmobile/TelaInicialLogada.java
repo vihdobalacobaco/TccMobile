33ae868db90963ca63b426b15251af6ac4de07c5
vihh <125580286+vihdobalacobaco@users.noreply.github.com>
2024-08-19 16:21:38 -0300

    Tela inicial logado

---
+++ app/src/main/java/com/example/tccmobile/TelaInicialLogada.java
@@ -0,0 +1,18 @@
+package com.example.tccmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class TelaInicialLogada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial_logada);
+
+
+    }
+}
