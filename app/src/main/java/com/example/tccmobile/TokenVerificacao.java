f12b6153cbe4ac5237bd6510ad416bd6d670a06f
vihdobalacobaco <vitoriajemima2007@gmail.com>
2024-09-05 11:02:57 -0300

    token de verificacao att

--- app/src/main/java/com/example/tccmobile/TokenVerificacao.java
+++ app/src/main/java/com/example/tccmobile/TokenVerificacao.java
@@ -2,13 +2,33 @@ package com.example.tccmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TokenVerificacao extends AppCompatActivity {
+
+    private TextView text_tenho_cad2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_token_verificacao);
+
+
+        IniciarComponentes();
+        text_tenho_cad2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TokenVerificacao.this, FormLogin.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes() {
     }
 }
