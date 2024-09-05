789af5f9fb281f540e3737dd6f844a31d3b3fbab
vihdobalacobaco <vitoriajemima2007@gmail.com>
2024-09-05 09:07:53 -0300

    osjocs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.tccmobile"
-        minSdk = 28
+        minSdk = 27
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
