ad2db8a4c6170c619dda5181db5ad0828fefcbf5
ed_002892 <edcarlos281282@gmail.com>
2024-09-27 08:17:17 -0300

    Bom dia,
    Consegui resolver o problema de conexão com o Banco, refiz todo o código e deu certo.
    
    Provavelmente deve ser algum autocompletar que não foi feito.
    
    Depois só mudar a parte do nome do Banco e IP da máquina.
    
    Bons estudos!

--- app/src/main/java/com/example/tccmobile/BancoDeDados.java
+++ app/src/main/java/com/example/tccmobile/BancoDeDados.java
@@ -8,23 +8,20 @@ import java.sql.SQLException;
 
 public class BancoDeDados {
 
-    public static Connection conectar (TesteConexaoBD testeConexaoBD) {
-        Connection conn = null;
-        try {
-            StrictMode.ThreadPolicy politica;
-            politica = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(politica);
-
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:jtds:sqlserver://172.19.1.250"+
-                    "databaseName=bdPortal360;user=sa;password=@ITB123456;");
-             return conn;
-        } catch (SQLException e) {
-            e.getMessage();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return conn;
+   public static Connection conectar(TesteConexaoBD testeConexaoBD){
+   Connection conn = null;
+   try {
+       StrictMode.ThreadPolicy politica;
+       politica = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+       StrictMode.setThreadPolicy(politica);
+       Class.forName("net.sourceforge.jtds.jdbc.Driver");
+       conn = DriverManager.getConnection("jdbc:jtds:sqlserver://172.19.0.64;" +
+               "databaseName=Banco_Android;user=sa;password=@ITB123456;");
+   } catch (SQLException e){
+       e.getMessage();
+   } catch (ClassNotFoundException e){
+       e.printStackTrace();
+   }
+return conn;
     }
-
 }
--- app/src/main/java/com/example/tccmobile/TesteConexaoBD.java
+++ app/src/main/java/com/example/tccmobile/TesteConexaoBD.java
@@ -16,21 +16,22 @@ public class TesteConexaoBD extends AppCompatActivity {
         setContentView(R.layout.activity_teste_conexao_bd);
 
         Connection conn = BancoDeDados.conectar(TesteConexaoBD.this);
-        BancoTeste = findViewById(R.id.BancoTeste);
+        BancoTeste = findViewById(R.id.textView);
 
         try {
-            if (conn != null){
+            if (conn != null) {
                 if (!conn.isClosed())
-                    BancoTeste.setText("CONEXAO REALIZADA COM SUCESSO");
+                    BancoTeste.setText("CONEXAO Realizada com sucesso");
                 else
-                    BancoTeste.setText("A CONEXAO ESTÁ FECHADA");
+                    BancoTeste.setText("A CONEXÃO ESTÁ FECHADA");
             } else {
                 BancoTeste.setText("CONEXAO NULA, NÃO REALIZADA");
             }
-        } catch (java.sql.SQLException ex){
+        } catch (java.sql.SQLException ex) {
             ex.printStackTrace();
-            BancoTeste.setText("CONEXÃO FALHOU!!\n" +
+            BancoTeste.setText("CONEXÃO FALHOU!!!\n" +
                     ex.getMessage());
         }
+
     }
 }
