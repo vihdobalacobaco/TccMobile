a1bc789f11db19c0e28779493b001c9f8f73846b
vihdobalacobaco <vitoriajemima2007@gmail.com>
2024-10-08 08:33:44 -0300

    banco de dados att

--- app/src/main/java/com/example/tccmobile/FormCadastro.java
+++ app/src/main/java/com/example/tccmobile/FormCadastro.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class FormCadastro extends AppCompatActivity {
+
+    EditText editNome, editEmail, editSenha;
+    Button btn_cadastrar;
 
     private TextView text_tenho_cad;
 
@@ -15,6 +22,30 @@ public class FormCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        editNome = (EditText) findViewById(R.id.editNome);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editSenha = (EditText) findViewById(R.id.editSenha);
+        btn_cadastrar = (Button) findViewById(R.id.btn_cadastrar);
+
+        btn_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View v){
+                Usuario user = new Usuario(
+                        editNome.getText().toString(),
+                        editEmail.getText().toString(),
+                        editSenha.getText().toString()
+                );
+
+                int res = UsuarioCrud.inserirUsuario(user, getBaseContext());
+                if (res <=0){
+                    Snackbar.make(btn_cadastrar, "Dados não inseridos!", Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(btn_cadastrar, "Dados Cadastrados com sucesso", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
 
         IniciarComponentes();
         text_tenho_cad.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +56,8 @@ public class FormCadastro extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
     private void IniciarComponentes(){
         text_tenho_cad = findViewById(R.id.text_tenho_cad);
--- app/src/main/java/com/example/tccmobile/UsuarioCrud.java
+++ app/src/main/java/com/example/tccmobile/UsuarioCrud.java
@@ -3,6 +3,7 @@ package com.example.tccmobile;
 import android.content.Context;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class UsuarioCrud {
 
@@ -14,6 +15,17 @@ public class UsuarioCrud {
             PreparedStatement pst = BancoDeDados.conectar(ctx).prepareStatement(
                     "Insert Into Usuario (nome, email, senha)" + "values (?,?,?)");
 
+            pst.setString(1, Usuario.getNome());
+            pst.setString(2, Usuario.getEmail());
+            pst.setString(3, Usuario.getSenha());
+
+            resposta = pst.executeUpdate();
+        } catch (SQLException e) {
+            e.getMessage();
         }
+        return resposta;
+    }
+
+    public static int inserirUsuario(Usuario user, Context baseContext) {
     }
 }
